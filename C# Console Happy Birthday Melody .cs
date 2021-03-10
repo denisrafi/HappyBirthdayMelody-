5e269d63d11c83f80a930486a462c4778b57f576
DenisRafi <62852838+DenisRafi@users.noreply.github.com>
2021-03-10 05:37:59 +0000

    Add files via upload

---
+++ C# Console Happy Birthday Melody .cs
@@ -0,0 +1,68 @@
+using System.Threading;
+using static System.Console;
+
+namespace _
+{
+    class ByDR
+    {
+        static void Main()
+        {
+            A1();
+        }
+        private static void A1()
+        {
+            Thread.Sleep(2000);
+            Beep(264, 125);
+            Thread.Sleep(250);
+            Beep(264, 125);
+            Thread.Sleep(125);
+            Beep(297, 500);
+            Thread.Sleep(125);
+            Beep(264, 500);
+            Thread.Sleep(125);
+            Beep(352, 500);
+            Thread.Sleep(125);
+            Beep(330, 1000);
+            Thread.Sleep(250);
+            Beep(264, 125);
+            Thread.Sleep(250);
+            Beep(264, 125);
+            Thread.Sleep(125);
+            Beep(297, 500);
+            Thread.Sleep(125);
+            Beep(264, 500);
+            Thread.Sleep(125);
+            Beep(396, 500);
+            Thread.Sleep(125);
+            Beep(352, 1000);
+            Thread.Sleep(250);
+            Beep(264, 125);
+            Thread.Sleep(250);
+            Beep(264, 125);
+            Thread.Sleep(125);
+            Beep(2642, 500);
+            Thread.Sleep(125);
+            Beep(440, 500);
+            Thread.Sleep(125);
+            Beep(352, 250);
+            Thread.Sleep(125);
+            Beep(352, 125);
+            Thread.Sleep(125);
+            Beep(330, 500);
+            Thread.Sleep(125);
+            Beep(297, 1000);
+            Thread.Sleep(250);
+            Beep(466, 125);
+            Thread.Sleep(250);
+            Beep(466, 125);
+            Thread.Sleep(125);
+            Beep(440, 500);
+            Thread.Sleep(125);
+            Beep(352, 500);
+            Thread.Sleep(125);
+            Beep(396, 500);
+            Thread.Sleep(125);
+            Beep(352, 1000);
+        }
+    }
+}
